9c8221e413e55f39b2aaf5764f6f3f9e7a3b8f63
Hajji wajdi <97055878+wajdi-22@users.noreply.github.com>
2024-03-18 16:24:54 +0100

    V2

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 from ultralytics import YOLO
 import time
+from multiprocessing import Pool
+
 # List of sample images to process
-img_list = [f'worldhealthstatistics/page_{i}.png' for i in range(1, 20)]
+img_list = [f'worldhealthstatistics/page_{i}.png' for i in range(1,11)]
+
 
 # Load the document segmentation model
-
 docseg_model = YOLO('https://huggingface.co/DILHTWD/documentlayoutsegmentation_YOLOv8_ondoclaynet/resolve/main/yolov8x-doclaynet-epoch64-imgsz640-initiallr1e-4-finallr1e-5.pt?download=true',task="detect")
-start=time.time()
-# Process the images with the model
-results = docseg_model(source=img_list, save=True, show_labels=True, show_conf=True, show_boxes=True)
-end=time.time()
-print("Time taken for processing: ",end-start)
+
+def process_image(img):
+    # Process the image with the model
+    result = docseg_model(source=img, save=True, show_labels=True, show_conf=True, show_boxes=True)
+    return result
+
+if __name__ == "__main__":
+    start=time.time()
+
+    # Create a pool of worker processes
+    with Pool() as p:
+        # Apply the function to each image in the list in parallel
+        results = p.map(process_image, img_list)
+
+    end=time.time()
+    print("Time taken for processing: ",end-start)
